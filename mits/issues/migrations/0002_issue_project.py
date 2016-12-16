87ee4b1bf9c2acd78f4fb349a485a7224812da9a
mijara <marjara35@gmail.com>
2016-12-16 17:50:34 -0300

    Fix possible issue

--- mits/issues/migrations/0002_issue_project.py
+++ mits/issues/migrations/0002_issue_project.py
@@ -17,7 +17,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='issue',
             name='project',
-            field=models.ForeignKey(default=0, on_delete=django.db.models.deletion.CASCADE, to='projects.Project'),
+            field=models.ForeignKey(default=1, on_delete=django.db.models.deletion.CASCADE, to='projects.Project'),
             preserve_default=False,
         ),
     ]
