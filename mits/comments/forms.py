98c1d92d2261b6c9e882a2728a7735f2d2750295
mijara <marjara35@gmail.com>
2017-01-04 21:45:44 -0300

    Implement comment replies.

--- mits/comments/forms.py
+++ mits/comments/forms.py
@@ -7,3 +7,9 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['text']
+
+
+class ReplyForm(forms.ModelForm):
+    class Meta:
+        model = Reply
+        fields = ['text']
